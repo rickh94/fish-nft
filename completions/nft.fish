38bc4904857002177d07f9156da5f66027968f9e
Rick Henry <fredericmhenry@gmail.com>
2018-04-04 14:48:59 -0400

    move some stuff to variables for better readability

--- completions/nft.fish
+++ completions/nft.fish
@@ -1,5 +1,9 @@
 # Completions for nft command line interface to nftables
 # Based heavily on the built in git completion in fish shell
+#
+
+set -l choices "chain" "table" "rule" "set" "element" "map"
+set -l families "ip" "ip6" "inet" "arp" "bridge" "netdev"
 
 # check if nft is waiting for a command
 function __nft_needs_command
@@ -45,15 +49,28 @@ end
 # check whether a family is needed
 function __nft_needs_family
   set -l cmd (commandline -opc)
-  for choice in "chain" "table" "rule" "set" "element" "map"
-    if [ "$cmd[-1]" = "$choice" ]
+  for choice in $choices
+    if [ $cmd[-1] = $choice ]
       return 0
     end
   end
   return 1
 end
 
+# check whether a table is needed
+function __nft_needs_table
+  set -l cmd (commandline -opc)
+  for word in $choices $families
+    if [ $cmd[-1] = $word ]
+      return 0
+    end
+  end
+  return 1
+end
+# nft must take a subcommand or a switch, file completions are useless here
 complete -c nft -f
+
+# Switches
 complete -c nft -s h -l help -x -d "Show help message and all options"
 complete -c nft -s v -l version -x -d "Show version"
 complete -c nft -s n -l numeric -d "Show data numerically"
@@ -65,6 +82,8 @@ complete -c nft -s e -l echo -d "When inserting items into the ruleset, print no
 complete -c nft -s I -l includepath -r -d "Add directory to the list of directories to be searched for included files."
 complete -c nft -s f -l file -r -d "Read input from a file"
 complete -c nft -s i -l interactive -x -d "Read input from an interactive readline CLI"
+
+# Subcommands
 complete -c nft -n "__nft_needs_command" -a add -d "Add a table, chain, rule, set, map, or object"
 complete -c nft -n "__nft_needs_command" -a list -d "List a ruleset, table, chain, set, map, or object"
 complete -c nft -n "__nft_needs_command" -a flush -d "Flush (delete everything from) a ruleset, table, chain, set, or map"
@@ -76,10 +95,14 @@ complete -c nft -n "__nft_needs_command" -a insert -d "Similar to the add comman
 complete -c nft -n "__nft_needs_command" -a replace -d "Similar to the add command, but replaces the specified rule."
 complete -c nft -n "__nft_needs_command" -a reset -d "List-and-reset stateful object."
 complete -c nft -n "__nft_needs_command" -a chain -d "Edit an existing chain."
+
+# command groups(ish). table/chain/rule/etc
 complete -c nft -n "__nft_needs_choice add delete" -a "table chain set rule map element"
-complete -c nft -n "__nft_using_command list" -a "ruleset tables chains sets maps table chain set map"
-complete -c nft -n "__nft_using_command flush" -a "ruleset table chain set map"
-complete -c nft -n "__nft_using_command export" -a "ruleset"
-complete -c nft -n "__nft_using_command create rename" -a "chain"
-complete -c nft -n "__nft_using_command insert replace" -a "rule"
-complete -c nft -n "__nft_needs_family" -a "ip ip6 inet arp bridge netdev"
+complete -c nft -n "__nft_needs_choice list" -a "ruleset tables chains sets maps table chain set map"
+complete -c nft -n "__nft_needs_choice flush" -a "ruleset table chain set map"
+complete -c nft -n "__nft_needs_choice export" -a "ruleset"
+complete -c nft -n "__nft_needs_choice create rename" -a "chain"
+complete -c nft -n "__nft_needs_choice insert replace" -a "rule"
+
+# after command groups
+complete -c nft -n "__nft_needs_family" -a "$families" -d "family (optional)"
